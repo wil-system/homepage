344de25957367edd3b3e47e011688e95773648b7
lallafm1984 <lallafm1984@gmail.com>
2026-04-17 16:11:17 +0900

    04170411

--- app/page.js
+++ app/page.js
@@ -7,10 +7,10 @@ export const metadata = {
   openGraph: {
     title: "라페어라운지",
     description: "라페어라운지는 프리미엄 언더웨어, 파자마, 라운지웨어 전문 쇼핑몰입니다. 라페어라운지에서 편안하고 스타일리시한 라페어라운지 제품을 만나보세요.",
-    url: "https://www.laffairlounge.co.kr",
+    url: "https://www.laffairlounge.kr",
     images: [
       {
-        url: "https://www.laffairlounge.co.kr/Images/main_img/main.webp",
+        url: "https://www.laffairlounge.kr/Images/main_img/main.webp",
         width: 1200,
         height: 630,
         alt: "라페어라운지 대표 이미지"
@@ -22,7 +22,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "라페어라운지",
     description: "라페어라운지는 프리미엄 언더웨어, 파자마, 라운지웨어 전문 쇼핑몰입니다. 라페어라운지에서 편안하고 스타일리시한 라페어라운지 제품을 만나보세요.",
-    images: ["https://www.laffairlounge.co.kr/Images/main_img/main.webp"]
+    images: ["https://www.laffairlounge.kr/Images/main_img/main.webp"]
   }
 }
 
